bb6d9790656bf6435cae451c15d8ebc9b00e00f1
bscotty <bryan@pajato.com>
2017-11-11 16:11:08 -0500

    Fixing a few things; adding an additional X button that will return you to the parent activity in the toolbar.

--- app/src/main/java/com/pajato/doubleactivities/SecondActivity.kt
+++ app/src/main/java/com/pajato/doubleactivities/SecondActivity.kt
@@ -1,12 +1,27 @@
 package com.pajato.doubleactivities
 
+import android.graphics.Color
+import android.graphics.PorterDuff
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class SecondActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second2)
+        setContentView(R.layout.activity_second)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        menu.getItem(0).icon.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        onBackPressed()
+        return super.onOptionsItemSelected(item)
     }
 }
